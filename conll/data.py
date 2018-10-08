c4034d6413113a249c6d094db978da77bb264a45
stephantul <stephan.tulkens@uantwerpen.be>
2018-10-08 10:04:48 +0200

    add extra data_readers

--- conll/data.py
+++ conll/data.py
@@ -9,27 +9,27 @@ These functions are convenience functions.
 import unicodedata
 import numpy as np
 
-from wordkit.corpora import Subtlex
-from wordkit.corpoa import Lexique
+from string import ascii_lowercase
+from wordkit.corpora import Subtlex, Lexique
 from .lexicon import read_blp_format, read_dlp2_format, read_flp_format
 
 
 # This path needs to be modified
-CORPUS_PREFIX = "data/"
+C_PREFIX = "data"
 
-# These paths need to be modified.
 CORPORA = {"nld": (Subtlex,
-                   "{}SUBTLEX-NL.cd-above2.txt".format(CORPUS_PREFIX),
+                   "{}/SUBTLEX-NL.cd-above2.txt".format(C_PREFIX),
                    read_dlp2_format,
-                   "{}dlp2_items.tsv".format(CORPUS_PREFIX)),
+                   "{}/dlp2_items.tsv".format(C_PREFIX)),
            "eng-uk": (Subtlex,
-                      "{}SUBTLEX-UK.xlsx".format(CORPUS_PREFIX),
+                      "{}/SUBTLEX-UK.xlsx".format(C_PREFIX),
                       read_blp_format,
-                      "{}blp-items.txt".format(CORPUS_PREFIX)),
+                      "{}/blp-items.txt".format(C_PREFIX)),
            "fra": (Lexique,
-                   "{}Lexique382.txt".format(CORPUS_PREFIX),
+                   "{}/Lexique382.txt".format(C_PREFIX),
                    read_flp_format,
-                   "{}French Lexicon Project words.xls".format(CORPUS_PREFIX))}
+                   "{}/French Lexicon Project words.xls"
+                   "".format(C_PREFIX))}
 
 FIELDS = ("orthography", "frequency", "log_frequency")
 
@@ -42,74 +42,34 @@ def normalize(string):
 
 def filter_function_ortho(x):
     """Filter words based on punctuation and length."""
-    a = not set(x['orthography']).intersection({' ',
-                                                "'",
-                                                '.',
-                                                '/',
-                                                ',',
-                                                '-'
-                                                '&',
-                                                '0',
-                                                '1',
-                                                '2',
-                                                '3',
-                                                '4',
-                                                '5',
-                                                '6',
-                                                '7',
-                                                '8',
-                                                '9',
-                                                '_',
-                                                '&',
-                                                '-',
-                                                '@'})
-    return a and len(x['orthography']) >= 2
+    a = not set(x['orthography']) - set(ascii_lowercase)
+    return a and len(x['orthography']) >= 2 and x['frequency'] > 1
 
 
 def load_data(language, max_num=np.inf):
-    """
-    Load words and the rt data.
-
-    Parameters
-    ----------
-    language : str
-        The language for which to load the data.
-    max_num : int or inf
-        The number of items to load. If set to inf, this is ignored.
-
-    Returns
-    -------
-    new_words : list of dicts
-        The information for each word in the corpus.
-    rt_data : dict
-        The RT data for each word.
-
-    """
+    """Load the words and the RT data."""
     reader, path, lex_func, lex_path = CORPORA[language]
 
     rt_data = lex_func(lex_path)
     rt_data = {normalize(k): v for k, v in rt_data}
     r = reader(path,
                language=language,
-               fields=FIELDS)
+               fields=FIELDS,
+               merge_duplicates=True,
+               scale_frequencies=False)
 
-    words = r.transform(filter_function=filter_function_ortho)
-    for x in words:
-        x['orthography'] = normalize(x['orthography'])
-
-    words = [x for x in words if len(x['orthography']) > 1]
-
-    temp = set()
-    words = sorted(words,
-                   key=lambda x: (x['frequency'], x['orthography']),
-                   reverse=True)
+    words = r.transform()
     new_words = []
+    seen = set()
     for x in words:
-        if x['orthography'] in temp:
+        x['orthography'] = normalize(x['orthography']).lower()
+        if x['orthography'] in seen:
             continue
-        temp.add(x['orthography'])
+        seen.add(x['orthography'])
         new_words.append(x)
-        if len(new_words) == max_num:
-            break
+    words = list(filter(filter_function_ortho, new_words))
+    ortho_forms = [x['orthography'] for x in words]
+    set_ortho = set(ortho_forms)
+    rt_data = {k: v for k, v in rt_data.items() if k in set_ortho}
 
-    return new_words, rt_data
+    return words, rt_data, [x for x in words if x['orthography'] in rt_data]
